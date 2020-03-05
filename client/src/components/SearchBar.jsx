876f43f2fbee91a5e74c4c7362bcf4ca7a58b08f
Dean Ma <mrdingma@gmail.com>
2020-03-04 20:43:35 -0800

    add search bar

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import { Button, Container, Row, Col, Form, Spinner } from "react-bootstrap";
+import { Icon } from "react-materialize";
+
+const SearchBar = props => {
+  const [text, setText] = useState(props.keyword);
+
+  const clickHandler = () => {
+    console.log(text);
+    props.setKeyword(text);
+  };
+
+  let content = (
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <Form.Group
+              controlId="exampleForm.ControlInput1"
+              style={{ display: "flex" }}
+            >
+              <div style={{ alignSelf: "center" }}>
+                <Icon medium>search</Icon>
+              </div>
+              <Form.Control
+                type="text"
+                style={{ borderRadius: "100px", width: "80%" }}
+                onChange={e => setText(e.target.value)}
+                value={text}
+              />
+              {props.isLoading ? (
+                <Button
+                  variant="primary"
+                  style={{ marginLeft: "15px" }}
+                  disabled
+                >
+                  <Spinner
+                    as="span"
+                    animation="grow"
+                    size="sm"
+                    role="status"
+                    aria-hidden="true"
+                  />
+                  Loading...
+                </Button>
+              ) : (
+                <Button
+                  as="input"
+                  type="submit"
+                  style={{ marginLeft: "15px", cursor: "pointer" }}
+                  value="Abe Search"
+                  onClick={clickHandler}
+                />
+              )}
+            </Form.Group>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+
+  return content;
+};
+
+export default SearchBar;
