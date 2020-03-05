f0184c98c74f7f93f49e4daf07ccb25bca3c02e2
Dean Ma <mrdingma@gmail.com>
2020-03-05 00:27:57 -0800

    pagination implemented

--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -1,17 +1,58 @@
 import React, { useState, useEffect } from "react";
 import EventEntry from "./EventEntry.jsx";
+import Pagination from "react-bootstrap/Pagination";
 
 const Events = ({ dataArr }) => {
-  // const [isDashboardView, setIsDashboardView] = useState();
+  const [currentPage, setCurrentPage] = useState(1);
 
-  console.log(dataArr);
-  let content = (
-    <>
-      {dataArr.length > 0
-        ? dataArr.map(event => <EventEntry event={event} />)
-        : null}
-    </>
-  );
+  const len = dataArr.length;
+  const maxPage = Math.ceil(len / 5);
+
+  const pageNumberArr = () => {
+    let result = [];
+
+    for (let i = 1; i <= maxPage; i++) {
+      result.push(i);
+    }
+    return result;
+  };
+
+  let content = <></>;
+
+  if (len > 0) {
+    content = (
+      <>
+        {dataArr.slice((currentPage - 1) * 5, currentPage * 5).map(event => (
+          <EventEntry event={event} />
+        ))}
+        <Pagination>
+          {currentPage !== 1 ? (
+            <>
+              <Pagination.First onClick={() => setCurrentPage(1)} />
+              <Pagination.Prev
+                onClick={() => setCurrentPage(currentPage - 1)}
+              />
+            </>
+          ) : null}
+          {pageNumberArr().map(number => {
+            if (number === currentPage) {
+              return <Pagination.Item active>{number}</Pagination.Item>;
+            }
+            return <Pagination.Item>{number}</Pagination.Item>;
+          })}
+          {currentPage !== maxPage ? (
+            <>
+              <Pagination.Next
+                onClick={() => setCurrentPage(currentPage + 1)}
+              />
+              <Pagination.Last onClick={() => setCurrentPage(maxPage)} />
+              />
+            </>
+          ) : null}
+        </Pagination>
+      </>
+    );
+  }
 
   return content;
 };
